b3d721c65283c8efff540022da86a298e518d38b
kribo852 <kristoffer.a.borg@gmail.com>
2025-12-18 18:27:14 +0100

    Ongoing rewrite to typescript
    An ongoing rewrite of the project, to typescript.
    
    To make the code better and to make it more accessible.

---
+++ run-robot.ts
@@ -0,0 +1,58 @@
+import {TreeNode, HtmlNode} from './symmetrical-octo-robot.js'
+import { writeFile } from 'fs';
+
+
+let tree = new HtmlNode(); 
+
+let grid = {
+	type: "layout",
+	grid: [
+		["", "heading", ""],
+		["first_name last_name telephone"],
+		["footer", ""]
+	]
+};
+
+tree.add(grid);
+
+
+let content = [
+	{
+		type: "input_text",
+		name: "first_name",
+		description: "enter your first name" 
+	},
+	{
+		type: "input_text",
+		name: "last_name",
+		description: "enter your last name" 
+	},
+	{
+		type: "input_text",
+		name: "telephone",
+		description: "enter your phone number" 
+	},
+	{
+		type: "information",
+		name: "heading",
+		description: "sign up for buying a near as new car!"
+	},
+	{
+		type: "information",
+		name: "footer",
+		description: "other crazy offers here, or call +5436 1122334455678912367543345435243267"
+	}
+];
+
+for (let index in content) {
+	tree.add(content[index]);
+}
+
+
+
+writeFile("output.html", tree.output,  function(err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("File created!");
+});
---
+++ symmetrical-octo-robot.ts
@@ -0,0 +1,194 @@
+
+export interface TreeNode {
+	startswith: string;
+	endswith: string;
+
+	get output(): string;
+
+	add(json: any);
+
+}
+
+export class HtmlNode implements TreeNode {
+	startswith: string;
+	endswith: string;
+	styleNode: StyleNode;
+	gridNode: GridNode;
+
+	constructor() {
+		this.startswith = "<!doctype html><html>";
+		this.endswith = "</html>";
+		this.styleNode = new StyleNode();
+		this.gridNode = new GridNode();
+	}
+
+	get output(): string {
+		return this.startswith + this.styleNode.output + "<body>"+this.gridNode.output+"</body>" + this.endswith;
+	}
+
+	add(json: any) {
+		this.gridNode.add(json);
+	}
+
+}
+
+class StyleNode implements TreeNode {
+	startswith: string;
+	endswith: string;
+
+	constructor() {
+		this.startswith = `<style>
+		.container { 
+			display: flex;
+			height: auto;
+		}
+		div.item {
+			width: 100%;
+			background-color: rgb(215,215,255);
+			border-right: 1px solid grey;
+			border-bottom: 1px solid grey;
+			text-align: center;
+		}
+		p.forinput{
+			margin-top:2px;
+			margin-bottom:2px;
+		}
+		input {
+			margin-bottom:2px;
+		}
+		body {
+			background-color: rgb(225,220,215);
+		}`;
+		this.endswith = "</style>";
+	}
+
+
+	get output(): string {
+		return this.startswith + this.endswith;
+	}
+
+	add(json: any) {
+
+	}
+}
+
+class GridNode implements TreeNode {
+	startswith: string;
+	endswith: string;
+	grid;
+	elements: Map<string,ElementNode>;
+
+	constructor() {
+		this.startswith = "";
+		this.endswith = "";
+		this.elements = new Map<string,ElementNode>();
+	}
+
+	get output(): string {
+		let rtn = "";
+		console.log(this.grid);
+
+		for (let row_index in this.grid) {
+			let row = this.grid[row_index];
+			rtn += "<div class=\"container\">";
+			for (let column_index in row) {
+				let lookupElementNames = row[column_index].split(" ");
+					for(let i in lookupElementNames) {
+						rtn += this.elements.get(lookupElementNames[i])?.output ?? "";
+					}
+			}
+			rtn += "</div>";
+		}
+
+		return this.startswith + rtn + this.endswith;
+	}
+
+	add(json: any) {
+		if(json.type == "layout") {
+			this.grid = json.grid;
+			return
+		}
+
+
+		let newPotentialNode = new ElementNode(); 
+		newPotentialNode.add(json);
+		if(newPotentialNode.isActivated) {
+			this.elements.set(json.name, newPotentialNode);
+		}
+	}
+}
+
+class ElementNode implements TreeNode {
+    contained: TreeNode;
+    startswith: string;
+	endswith: string;
+
+    constructor() {
+    	this.startswith = `<div class="item">`;
+    	this.endswith = `</div>`;
+    }
+
+    add(json: any) {
+    	if (json.type == "input_text") {
+    		this.contained = new InputTextNode();
+    		this.contained.add(json);
+    	}
+    	if (json.type == "information") {
+    		this.contained = new InformationNode();
+    		this.contained.add(json);
+    	}
+    }
+
+    get output(): string {
+    	return this.startswith + this.contained.output + this.endswith;
+    }
+
+    get isActivated(): boolean {
+    	return !!this.contained;
+    }
+
+}
+
+class InputTextNode implements TreeNode {
+	startswith: string;
+	endswith: string;
+
+
+	constructor() {
+    	this.startswith = `<div><p class="forinput">`;
+    	this.endswith = `</p><input></div>`;
+    }
+
+    add(json: any) {
+    	this.startswith += json.description;
+    }
+
+    get output(): string {
+    	return this.startswith + this.endswith;
+    }
+
+}
+
+
+class InformationNode implements TreeNode {
+	startswith: string;
+	endswith: string;
+
+
+	constructor() {
+    	this.startswith = `<p class="forinput">`;
+    	this.endswith = `</p>`;
+    }
+
+    add(json: any) {
+    	this.startswith += json.description;
+    }
+
+    get output(): string {
+    	return this.startswith + this.endswith;
+    }
+
+}
+
+
+
